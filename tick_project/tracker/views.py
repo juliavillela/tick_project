8ac1430af86fb1bc3c10d4234ad2b651d1ac91e5
juliavillela <julia.arruda@gmail.com>
2025-06-25 19:23:14 -0300

    add task counter to index view

--- tick_project/tracker/views.py
+++ tick_project/tracker/views.py
@@ -10,10 +10,16 @@ def index(request):
     context["tasks"] = tasks
     context["projects"] = Project.objects.filter(user = request.user)
     today_sessions = []
+    today_tasks = []
     for task in tasks:
+        if task.is_done:
+            if task.done_at.date() == timezone.now().date():
+                today_tasks.append(task)
         for session in task.sessions.all():
             if session.start_time.date() == timezone.now().date():
                 today_sessions.append(session)
     today_time = sum(session.duration_in_seconds() for session in today_sessions)
+
     context["today_time"] = timedelta(seconds=today_time)
+    context ["today_tasks"] = len(today_tasks)
     return render(request, template, context)
