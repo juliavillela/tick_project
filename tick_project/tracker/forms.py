15492631159724a00c38320f544b2efd0ab60892
juliavillela <julia.arruda@gmail.com>
2025-06-29 14:16:13 -0300

    update task form to inclue is_done field

--- tick_project/tracker/forms.py
+++ tick_project/tracker/forms.py
@@ -5,12 +5,17 @@ from .models import Task
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ["project", "name"]
+        fields = ["project", "name", "is_done"]
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
         self.fields["project"].queryset = user.project_set.all()
+        # set label for is_done field
+        self.fields["is_done"].label = "Done"
         # set form-control bs class to fields
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
+        for name, field in self.fields.items():
+            if name in ["project", "name"]:
+                field.widget.attrs.update({'class': 'form-control'})
+            elif name == "is_done":
+                field.widget.attrs.update({'class': 'form-check-input'})
