c6f865510b92a0ee1f930b7d7716ad41ec10e353
juliavillela <julia.arruda@gmail.com>
2025-06-29 18:09:06 -0300

    update tracker js function to include hours

--- tick_project/static/js/track.js
+++ tick_project/static/js/track.js
@@ -1,11 +1,9 @@
-window.addEventListener('DOMContentLoaded', () =>{
     console.log("runing tracker.js")
     const timerElement = document.getElementById("timer")
     const startTime = timerElement.dataset.startTime
     if (startTime != 0){
         startTimer(startTime)
     }
-})
 
 function startTimer(initialTime){
     console.log("starting timer from", initialTime)
@@ -16,9 +14,10 @@ function startTimer(initialTime){
     function updateTimer() {
     const now = Date.now();
     const elapsed = Math.floor((now - startTime) / 1000);
-    const minutes = String(Math.floor(elapsed / 60)).padStart(2, '0');
+    const hours = String(Math.floor(elapsed / 3600)).padStart(2, '0');
+    const minutes = String(Math.floor((elapsed % 3600) / 60)).padStart(2, '0');
     const seconds = String(elapsed % 60).padStart(2, '0');
-    timerEl.textContent = `${minutes}:${seconds}`;
+    timerEl.textContent = `${hours}:${minutes}:${seconds}`;
     }
 
     setInterval(updateTimer, 1000);
