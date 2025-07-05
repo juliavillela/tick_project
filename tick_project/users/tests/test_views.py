d710d0f19142d6af9469b4bf52551c09fc9d6de5
juliavillela <julia.arruda@gmail.com>
2025-07-05 19:43:22 -0300

    create login view tests

--- tick_project/users/tests/test_views.py
+++ tick_project/users/tests/test_views.py
@@ -6,7 +6,7 @@ from tracker.models import Project
 
 User = get_user_model()
 
-class RegisterViewTeste(TestCase):
+class RegisterViewTest(TestCase):
     def setUp(self):
         self.url = reverse("users:register")  
         self.default_project_name = "General"
@@ -38,3 +38,46 @@ class RegisterViewTeste(TestCase):
 
         # Check redirect
         self.assertRedirects(response, reverse("tracker:dashboard"))
+
+class LoginViewTests(TestCase):
+    def setUp(self):
+        self.email = "user@example.com"
+        self.password = "securepassword123"
+        self.user = User.objects.create_user(
+            email=self.email,
+            password=self.password
+        )
+        self.url = reverse("users:login")  # adjust if needed
+
+    def test_login_view_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "login.html")
+
+    def test_login_view_post_valid_credentials(self):
+        response = self.client.post(self.url, {
+            "username": self.email,
+            "password": self.password,
+        })
+
+        # Check redirect
+        self.assertRedirects(response, reverse("tracker:dashboard"))
+
+        # Check user is logged in
+        user = get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+        self.assertEqual(user.email, self.email)
+
+    def test_login_view_post_invalid_credentials(self):
+        response = self.client.post(self.url, {
+            "username": self.email,
+            "password": "wrongpassword",
+        })
+
+        # Should stay on login page
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "login.html")
+
+        # User should not be logged in
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
