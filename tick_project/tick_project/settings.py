3505a8361919ae0a9d2dbf6744d49c7c66bf13f2
juliavillela <julia.arruda@gmail.com>
2025-08-20 18:16:01 -0300

    add pythonanywhere to allowed hosts

--- tick_project/tick_project/settings.py
+++ tick_project/tick_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juliavillela.pythonanywhere.com']
 
 
 # Application definition
